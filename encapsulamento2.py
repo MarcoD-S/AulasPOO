7c34554fce8397a8fad582a1a12d6c287cf8eaff
MarcoD-S <joaomarcoduarte149@gmail.com>
2025-11-28 10:47:46 -0300

    Update encapsulamento2.py

--- encapsulamento2.py
+++ encapsulamento2.py
@@ -1,7 +1,10 @@
 class Pessoa:
-    def __init__(self, nome = None, CPF = 0):
+    def __init__(self, nome = None, CPF = None):
+        if CPF is None:
+            CPF=input("Digite o CPF: ")
         self.nome = nome
         self.__CPF = CPF
+
 
     @property
     def CPF(self):
@@ -14,4 +17,14 @@ class Pessoa:
 
         else:
             self.__CPF = digitos
+
+    def digitar(self, digitos):
+        if 0 > len(digitos) < 11:
+            self.__digitos += digitos
+
+pessoa2 = Pessoa()
+print("CPF:", pessoa2.CPF)
+
+pessoa2.CPF = "12345678900"
+print("CPF após tentativa externa:", pessoa2.CPF)
 
